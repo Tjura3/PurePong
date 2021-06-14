60fd699a00037d6e8f4623e989e8f9a014aa9cd7
Thomas <1707235@mukilteo.wednet.edu>
2021-06-13 20:13:37 -0700

    Update Ball.png, Ball.png.meta, and 11 more files...

---
+++ Assets/Scripts/BallMove.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    readonly float LEFTRIGHT = 7.75f;
+
+    readonly float TOPBOTTOM = 4.25f;
+
+    Vector2 speed = new Vector2(4, -4);
+
+    bool ballServed = false;
+
+ 
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && !ballServed)
+        {
+            ballServed = true;
+            StartCoroutine(Run());
+        }
+    }
+
+    IEnumerator Run()
+    {
+        while (ballServed)
+        {
+
+
+
+            Vector3 delta = speed * Time.deltaTime;
+            Vector3 newPos = transform.position + delta;
+
+            if (newPos.x < -LEFTRIGHT)
+            {
+                newPos.x = -LEFTRIGHT;
+                speed.x *= -1;
+            }
+            else if (newPos.x > LEFTRIGHT)
+            {
+                newPos.x = LEFTRIGHT;
+                speed.x *= -1;
+            }
+            else if (newPos.y < -TOPBOTTOM)
+            {
+                newPos.y = -TOPBOTTOM;
+                speed.y *= -1;
+            }
+            else if (newPos.y > TOPBOTTOM)
+            {
+                newPos.y = TOPBOTTOM;
+                speed.y *= -1;
+
+            }
+            transform.position = newPos;
+
+            yield return new WaitForEndOfFrame();
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            speed.y *= -1;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Movepaddle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movepaddle : MonoBehaviour
+{
+    readonly float EDGE = 6.5f;
+
+    float speed = 6;
+
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        float delta = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+        pos.x += delta;
+
+        if (pos.x < -EDGE)
+        {
+            pos.x = -EDGE;
+        }
+        else if (pos.x > EDGE)
+        {
+            pos.x = EDGE;
+        }
+
+        transform.position = pos;
+
+    }
+
+}
