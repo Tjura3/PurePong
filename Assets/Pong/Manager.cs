9826919ebe9051536ff86c6112dd6b66e1efbad3
MUKILTEO\1707235 <1707235@mukilteo.wednet.edu>
2022-06-13 10:25:46 -0700

    no message

---
+++ Assets/Pong/Manager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    private static Texture2D _staticRectTexture;
+    private static GUIStyle _staticRectStyle;
+
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static void GUIDrawRect()
+    {
+
+    }
+    public static void GUIDrawRect(Rect position, Color color)
+    {
+
+    }
+
+}
